8306446e883e85031bcf8c8b61c9a4dbdf0700c9
zakrnem <zakrnem@zohomail.com>
2024-02-15 13:42:23 -0600

    Increase max post length

--- models/comment.js
+++ models/comment.js
@@ -5,7 +5,7 @@ const commentSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
   parent: { type: Schema.Types.ObjectId, ref: "Comment" },
-  message: { type: String, required: true, minLength: 10, maxLength: 1000 },
+  message: { type: String, required: true, minLength: 10, maxLength: 2000 },
   createdAt: { type: Date, default: Date.now },
 });
 
