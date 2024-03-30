b135f42692e6494aa24b83dd7a4181429a1c1ce9
zakrnem <zakrnem@zohomail.com>
2024-03-30 12:09:39 -0600

    Check if user is admin on admin methods

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -2,6 +2,7 @@ import asyncHandler from "express-async-handler";
 import User from "../models/user";
 import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
+import user from "../models/user";
 dotenv.config();
 
 const signup = asyncHandler(async (req, res) => {
@@ -42,6 +43,32 @@ const login = asyncHandler(async (req, res) => {
   }
 });
 
+const adminLogin = asyncHandler(async (req, res) => {
+  const user = await User.findOne({ username: req.body.username });
+  if (user) {
+    if (user.admin) {
+      const match = await bcrypt.compare(req.body.password, user.password);
+      if (match) {
+        req.session.userId = user._id;
+        res.cookie("session-cookie", req.sessionID, {
+          secure: process.env.NODE_ENV === "production",
+          httpOnly: false,
+          maxAge: 30 * 60 * 1000,
+        });
+        res
+          .status(200)
+          .json({ message: `Succesfully logged in ${user.fullname}` });
+      } else {
+        res.status(400).json({ message: "Password is wrong" });
+      }
+    } else {
+      res.status(400).json({ message: "User is not admin" });
+    }
+  } else {
+    res.status(400).json({ message: "Username is wrong" });
+  }
+});
+
 const logout = asyncHandler(async (req, res) => {
   if (!!req.session.userId) {
     req.session.destroy();
@@ -55,6 +82,12 @@ const check_auth = asyncHandler(async (req, res) => {
   res.status(200).json(isAuthenticated);
 });
 
+const check_admin_auth = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.session.userId);
+  const isAuthenticated = !!req.session.userId && user.admin;
+  res.status(200).json(isAuthenticated);
+});
+
 const is_authenticated = (req, res, next) => {
   if (req.session.userId) {
     next();
@@ -62,6 +95,19 @@ const is_authenticated = (req, res, next) => {
     res.status(401).send("Unauthorized, please log in.");
   }
 };
+
+const admin_auth = asyncHandler(async (req, res, next) => {
+  if (req.session.userId) {
+    const user = await User.findById(req.session.userId);
+    if (user.admin) {
+      next();
+    } else {
+      res.status(401).send("User is not admin");
+    }
+  } else {
+    res.status(401).send("Unauthorized, please log in.");
+  }
+});
 
 const get_user = asyncHandler(async (req, res) => {
   const userId = req.session.userId;
@@ -77,8 +123,11 @@ const get_user = asyncHandler(async (req, res) => {
 export default {
   signup,
   login,
+  adminLogin,
   logout,
   check_auth,
+  check_admin_auth,
   is_authenticated,
+  admin_auth,
   get_user,
 };
--- routes/admin.js
+++ routes/admin.js
@@ -6,67 +6,67 @@ import session_controller from "../controllers/sessionController";
 
 router.get(
   "/posts",
-  session_controller.is_authenticated,
+  session_controller.admin_auth,
   post_controller.admin_posts_get,
 );
 
 router.post(
   "/posts",
-  session_controller.is_authenticated,
+  session_controller.admin_auth,
   post_controller.admin_create_post,
 );
 
 router.get(
   "/posts/page/:id",
-  session_controller.is_authenticated,
+  session_controller.admin_auth,
   post_controller.admin_posts_page,
 );
 
 router.get(
   "/posts/:id",
-  session_controller.is_authenticated,
+  session_controller.admin_auth,
   post_controller.admin_post_get,
 );
 
 router.put(
   "/posts/:id",
-  session_controller.is_authenticated,
+  session_controller.admin_auth,
   post_controller.admin_post_update,
 );
 
 router.delete(
   "/posts/:id",
-  session_controller.is_authenticated,
+  session_controller.admin_auth,
   post_controller.admin_post_delete,
 );
 
 router.get(
   "/comments/:id",
-  session_controller.is_authenticated,
+  session_controller.admin_auth,
   comment_controller.comment_get,
 );
 
 router.delete(
   "/comments/:id",
-  session_controller.is_authenticated,
+  session_controller.admin_auth,
   comment_controller.comment_delete,
 );
 
 router.get(
   "/comments",
-  session_controller.is_authenticated,
+  session_controller.admin_auth,
   comment_controller.get_comments,
 );
 
-router.post("/login", session_controller.login);
+router.post("/login", session_controller.adminLogin);
 
 router.post(
   "/logout",
-  session_controller.is_authenticated,
+  session_controller.admin_auth,
   session_controller.logout,
 );
 
-router.get("/is_auth", session_controller.check_auth);
+router.get("/is_auth", session_controller.check_admin_auth);
 
 router.post("/login", session_controller.login);
 
